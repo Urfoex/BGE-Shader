725286f8b23bdd9172f7afeb0683a679bf3c6aba
Manuel Bellersen <BellersenM@googlemail.com>
2013-03-02 02:10:34 +0100

    Move paths to global object

--- GLSLShaderRepository.py
+++ GLSLShaderRepository.py
@@ -17,70 +17,67 @@ bl_info = {
 }
 
 
+gRepoObjects = {}
+gRepoObjects['blend_file'] = os.path.basename(bpy.data.filepath)
+gRepoObjects['blend_path'] = os.path.dirname(bpy.data.filepath)
+gRepoObjects['repository'] = "https://bitbucket.org/Urfoex/bge-shader"
+gRepoObjects['repository_folder'] = "bge-shader"
+gRepoObjects['repository_path'] = gRepoObjects['blend_path'] + os.sep + gRepoObjects['repository_folder']
+gRepoObjects['template_path'] = "startup" + os.sep + "bl_ui"
+gRepoObjects['template_file'] = "space_text.py"
+gRepoObjects['template_src'] = gRepoObjects['repository_path'] + os.sep + gRepoObjects['template_file']
+gRepoObjects['template_dest'] = bpy.utils.script_paths(gRepoObjects['template_path'])[0] + os.sep + gRepoObjects['template_file']
+
+
 class GLSLShaderRepository(bpy.types.Operator):
     bl_idname = "wm.import_glsl_shader_repository"
     bl_label = "Import GLSL Shader Repository"
     bl_options = {'REGISTER'}
 
-    repository = "https://bitbucket.org/Urfoex/bge-shader"
-    repository_folder = "bge-shader"
-    template_file = "space_text.py"
-    template_path = "startup" + os.sep + "bl_ui" + os.sep
-
-    #def __init__(self):
-        #return
-
     def __del__(self):
         import shutil
-        print("Removing repo:", self.repository_path)
+        print("Removing repo:", gRepoObjects['repository_path'])
         #subprocess.call(args=['rm', "-r", self.repository_path])
         print("(STUB)")
 
-        temp = bpy.utils.script_paths(self.template_path)[0]
-        print("Restoring:", temp + os.sep + self.template_file)
-        shutil.copy2(src=self.repository_path + os.sep + self.template_file + ".orig", dst=temp + os.sep + self.template_file)
+        src_file = gRepoObjects['template_src'] + ".orig"
+        print("Restoring:", gRepoObjects['template_dest'], "from:", src_file)
+        shutil.copy2(src=src_file, dst=gRepoObjects['template_dest'])
 
     def execute(self, context):
+        print("_-execute-_")
         import time
         start_time = time.clock()
         print("execute started at:", start_time)
-
+        self.run()
         print("execute finished at:", time.clock())
         print("took about", time.clock() - start_time, "s")
         return {'FINISHED'}
 
     def invoke(self, context, event):
-        print("Invoke:", event)
-        self.setPaths()
-        self.run()
+        self.execute(context)
         return {'RUNNING_MODAL'}
-
-    def setPaths(self):
-        self.blend_file = os.path.basename(bpy.data.filepath)
-        self.blend_path = os.path.dirname(bpy.data.filepath)
-        self.repository_path = self.blend_path + os.sep + self.repository_folder
-        #self.script_path = bpy.utils.script_paths(self.repository_folder)
 
     def run(self):
         retCode = 1
-        if not os.path.isdir(self.repository_path):
-            print("Cloning:", self.repository, "to:", self.repository_path, "...")
+        if not os.path.isdir(gRepoObjects['repository_path']):
+            print("Cloning:", gRepoObjects['repository'], "to:", gRepoObjects['repository_path'], "...")
             retCode = self.CloneRepository()
         else:
-            print("Updating repository at:", self.repository_path, "...")
+            print("Updating repository at:", gRepoObjects['repository_path'], "...")
             retCode = self.UpdateRepository()
         if retCode != 0:
-            print("Please check the repository at:", self.repository_path)
+            print("Please check the repository at:", gRepoObjects['repository_path'])
         else:
             import shutil
-            temp = bpy.utils.script_paths("startup" + os.sep + "bl_ui")[0]
-            shutil.copy2(src=self.repository_path + os.sep + self.template_file, dst=temp)
+            print("Modifying template file at:", gRepoObjects['template_dest'])
+            shutil.copy2(src=gRepoObjects['template_src'], dst=gRepoObjects['template_dest'])
 
     def CloneRepository(self):
-        return subprocess.call(args=['hg', 'clone', self.repository], cwd=self.blend_path)
+        return subprocess.call(args=['hg', 'clone', gRepoObjects['repository']], cwd=gRepoObjects['blend_path'])
 
     def UpdateRepository(self):
-        return subprocess.call(args=['hg', 'update'], cwd=self.repository_path)
+        return subprocess.call(args=['hg', 'update'], cwd=gRepoObjects['repository_path'])
 
 
 def menu_func(self, context):
