7240c89b08d84d4351404becf83efa59e2750d21
Manuel Bellersen <BellersenM@googlemail.com>
2013-03-02 01:08:07 +0100

    Switching template file with Addon on-off

--- GLSLShaderRepository.py
+++ GLSLShaderRepository.py
@@ -24,13 +24,21 @@ class GLSLShaderRepository(bpy.types.Operator):
 
     repository = "https://bitbucket.org/Urfoex/bge-shader"
     repository_folder = "bge-shader"
+    template_file = "space_text.py"
+    template_path = "startup" + os.sep + "bl_ui" + os.sep
 
     #def __init__(self):
         #return
 
     def __del__(self):
+        import shutil
         print("Removing repo:", self.repository_path)
-        subprocess.call(args=['rm', "-r", self.repository_path])
+        #subprocess.call(args=['rm', "-r", self.repository_path])
+        print("(STUB)")
+
+        temp = bpy.utils.script_paths(self.template_path)[0]
+        print("Restoring:", temp + os.sep + self.template_file)
+        shutil.copy2(src=self.repository_path + os.sep + self.template_file + ".orig", dst=temp + os.sep + self.template_file)
 
     def execute(self, context):
         import time
@@ -51,20 +59,28 @@ class GLSLShaderRepository(bpy.types.Operator):
         self.blend_file = os.path.basename(bpy.data.filepath)
         self.blend_path = os.path.dirname(bpy.data.filepath)
         self.repository_path = self.blend_path + os.sep + self.repository_folder
+        #self.script_path = bpy.utils.script_paths(self.repository_folder)
 
     def run(self):
+        retCode = 1
         if not os.path.isdir(self.repository_path):
             print("Cloning:", self.repository, "to:", self.repository_path, "...")
-            self.CloneRepository()
+            retCode = self.CloneRepository()
         else:
             print("Updating repository at:", self.repository_path, "...")
-            self.UpdateRepository()
+            retCode = self.UpdateRepository()
+        if retCode != 0:
+            print("Please check the repository at:", self.repository_path)
+        else:
+            import shutil
+            temp = bpy.utils.script_paths("startup" + os.sep + "bl_ui")[0]
+            shutil.copy2(src=self.repository_path + os.sep + self.template_file, dst=temp)
 
     def CloneRepository(self):
-        subprocess.call(args=['hg', 'clone', self.repository], cwd=self.blend_path)
+        return subprocess.call(args=['hg', 'clone', self.repository], cwd=self.blend_path)
 
     def UpdateRepository(self):
-        subprocess.call(args=['hg', 'update'], cwd=self.repository_path)
+        return subprocess.call(args=['hg', 'update'], cwd=self.repository_path)
 
 
 def menu_func(self, context):
